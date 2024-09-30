e3c68d0995eba885a17845b66fe30ae31024bf3f
Trinh Vo <uhttv@sfbcic.com>
2024-09-30 14:26:40 -0500

    Create Action

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -5,12 +5,33 @@ namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
-        // GET: Movies/Random (Controller/Action)
+        // GET: https://localhost:7126/Controller/Action/Id
+        // An action will return anything,includes a view (html/page)
+
+        // https://localhost:7126/Movie/Random
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
 
             return View(movie);
+        }
+
+        // https://localhost:7126/Movie/Edit/1
+        public IActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        // https://localhost:7126/Movie?pageIndex=2&sortBy=Date
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
         }
     }
 }
