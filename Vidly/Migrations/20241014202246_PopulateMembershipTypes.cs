926a40e4a7ee3f258842d0e6c7469d918097f9cd
Trinh Vo <uhttv@sfbcic.com>
2024-10-14 15:42:40 -0500

    Seeding the Database

---
+++ Vidly/Migrations/20241014202246_PopulateMembershipTypes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Vidly.Models;
+
+#nullable disable
+
+namespace Vidly.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateMembershipTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MembershipType",
+                columns: new[] { nameof(MembershipType.Id), nameof(MembershipType.SignUpFee), nameof(MembershipType.DurationInMonths), nameof(MembershipType.DiscountRate) },
+                values: new object[,]
+                {
+                    {1, 0, 0, 0},
+                    {2, 30, 1, 10},
+                    {3, 90, 3, 15},
+                    {4, 300, 12, 20},
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
