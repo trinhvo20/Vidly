2eed7c12c8dca89b896c8331f6646155cdb2a33e
Trinh Vo <uhttv@sfbcic.com>
2024-10-14 15:47:29 -0500

    Overriding Conventions

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,12 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int? Id { get; set; }
-        public string? Name { get; set; }
+        [StringLength(255)]
+        public required string Name { get; set; }
         public bool? IsSubscribedToNewsLetter { get; set; }
         public MembershipType? MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
