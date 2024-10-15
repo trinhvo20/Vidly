b3327a84ee15b729f1b8df4bb889024261e47cdc
Trinh Vo <uhttv@sfbcic.com>
2024-10-15 16:35:54 -0500

    Styling Validation Error

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -4,14 +4,14 @@ namespace Vidly.Models
 {
     public class Customer
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         
         [StringLength(255)]
         public required string Name { get; set; }
         
         public bool IsSubscribedToNewsLetter { get; set; }
         
-        public MembershipType? MembershipType { get; set; }
+        public MembershipType MembershipType { get; set; }
 
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
