fd128f801d85feae07c6bae84bb9c4e91ceb8cf2
Trinh Vo <uhttv@sfbcic.com>
2024-10-21 11:16:17 -0500

    - Add property NumberAvailable to Movies table to check if we have enough number of that movie in stock for rental. - Check edge cases for rental movie action.

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -18,13 +18,32 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IActionResult CreateNewRentals(NewRentalDto newRentalDto)
         {
+            // If no movie in the DTO (customer did not rent any movie)
+            if (newRentalDto.MovieIds.Count == 0)
+                return BadRequest("New Movie Ids have been given.");
+            
             // Get the customer and list of movies from DB
-            var customerInDB = _context.Customers.Single(c => c.Id == newRentalDto.CustomerId);
-            var moviesInDB = _context.Movies.Where(m => newRentalDto.MovieIds.Contains(m.Id));
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == newRentalDto.CustomerId);
+            
+            if (customerInDB == null)
+                return BadRequest("CustomerId is not valid.");
+
+            var moviesInDB = _context.Movies.Where(m => newRentalDto.MovieIds.Contains(m.Id)).ToList();
+
+            if (moviesInDB.Count != newRentalDto.MovieIds.Count)
+                return BadRequest("One or more Movie Ids are invalid.");
 
             // For each movie rented, create a Rental object, add it to the Rental table.
             foreach (var movie in moviesInDB)
             {
+                // Check if that movie is enough NumberAvailable for renting.
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                // Reduce the NumberAvailable for that movie
+                movie.NumberAvailable--;
+
+                // Create a Rental object
                 var rental = new Rental()
                 {
                     Customer = customerInDB,
@@ -32,6 +51,7 @@ namespace Vidly.Controllers.Api
                     DateRented = DateTime.Now
                 };
 
+                // Add Rental object to DB
                 _context.Rentals.Add(rental);
             }
 
---
+++ Vidly/Migrations/20241021155439_AddNumberAvailablePropInMoviesTable.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNumberAvailablePropInMoviesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Customer",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "Movie",
+                table: "Rentals");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovieId",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "NumberAvailable",
+                table: "Movies",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_CustomerId",
+                table: "Rentals",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_MovieId",
+                table: "Rentals",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Customers_CustomerId",
+                table: "Rentals",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Movies_MovieId",
+                table: "Rentals",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            // My custom query to set NumberInStock = NumberAvailable when a movie is first created in Movies table.
+            migrationBuilder.Sql(@"UPDATE Movies SET NumberAvailable = NumberInStock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Customers_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Movies_MovieId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_MovieId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "MovieId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "NumberAvailable",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Customer",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Movie",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
